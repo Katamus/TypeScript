460685ccca8d5bbf658b36aa06a288fc065047cf
Christian <christiancatamuscay@r-fast.com>
2023-06-29 15:57:58 -0500

    S3 - Tipos basicos : 18. Arrays - Arreglos

---
+++ bases/tipos/arrays.ts
@@ -0,0 +1,15 @@
+(()=>{
+
+    // const numbers: (string|number|boolean)[] = [1,2,3,4,5,6];
+    const numbers: number[] = [1,2,3,4,5,6];
+
+    const villian = ['Omega Rojo','Dormamu','Duende Verde']
+    
+    numbers.push(11);
+
+    villian.forEach(v=> console.log(v.toUpperCase()));
+
+    console.log(numbers);
+    
+
+})();
