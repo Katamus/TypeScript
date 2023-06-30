3102d98bbf017438976109a7edf2d58ba2f83ee6
Christian <christiancatamuscay@r-fast.com>
2023-06-30 08:01:44 -0500

    S4 - Funciones y objetos : 27. Funciones básicas

---
+++ bases/funciones/function.js
@@ -0,0 +1,15 @@
+"use strict";
+(() => {
+    const hero = 'Flash';
+    function returnName() {
+        return hero;
+    }
+    const activateBatiSignal = () => {
+        return 'Batiseñal';
+    };
+    
+   console.log(typeof activateBatiSignal());
+
+   const heroName = returnName();
+    
+})();
---
+++ bases/funciones/function.ts
@@ -0,0 +1,15 @@
+(()=>{
+    const hero: string = 'Flash';
+
+    function returnName():string{
+        return hero;
+    }
+
+    const activateBatiSignal = ():string =>{
+        return 'Batiseñal'
+    }
+
+    returnName();
+    activateBatiSignal();
+
+})();
