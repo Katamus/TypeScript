4ff14c91d44014a3c69a8b7b757963accdd45b17
Christian <christiancatamuscay@r-fast.com>
2023-07-01 17:57:55 -0500

    S8 - Clases en TypeScript : 58. Definición de una clase básica en TypeScript

--- bases/main.js
+++ bases/main.js
@@ -1,52 +1,14 @@
 "use strict";
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
+(() => {
+    class Avenger {
+        constructor(name, team, realName) {
+            this.name = name;
+            this.team = team;
+            this.realName = realName;
         }
-    return t;
-};
-(() => {
-    const avengers = {
-        nick: 'Samuel L. Jackson',
-        ironman: 'Robert Downey Jr',
-        vision: 'Paul Bettany',
-        active: true,
-        poder: 1500.123123
-    };
-    const printAvenger = (_a) => {
-        var { ironman } = _a, rest = __rest(_a, ["ironman"]);
-        console.log(avengers.vision, rest);
-    };
-    const avengersArr = ['Cap. America', true, 150.15];
-    const [capi, ironman,] = avengersArr;
-    console.log({ ironman, capi });
-})();
-(() => {
-    const ironman = {
-        name: 'Ironman',
-        weapon: 'ArmorSuit'
-    };
-    const captainAmerica = {
-        name: 'Capitan Ameroca',
-        weapon: 'Escudo'
-    };
-    const thor = {
-        name: 'Thor',
-        weapon: 'Mjolnir'
-    };
-    const avengers = [ironman, thor, captainAmerica];
-    for (const avenger of avengers) {
-        console.log(avenger);
     }
-})();
-(() => {
-    const nombre = 'Christian';
-    const getName = () => {
-    };
+    Avenger.avgAge = 35;
+    const antman = new Avenger('Antman', 'Capitan');
+    console.log(antman.realName, Avenger.avgAge);
 })();
 //# sourceMappingURL=main.js.map
