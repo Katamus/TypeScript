104203236e65e565405a1644ae659a8862e33a6c
Christian <christiancatamuscay@r-fast.com>
2023-07-08 11:43:08 -0500

    S11 - Genericos - Generics: 84. Funciones Genéricas

--- ts-node/src/generics/generics.ts
+++ ts-node/src/generics/generics.ts
@@ -3,6 +3,8 @@ export const printObject  = (object:any) =>{
 }
 
 
-export function genericFunction (argument:any){
+export function genericFunction<T> (argument:T):T{
     return argument;
 }
+
+export const  genericFunctionArrow = <T> (argument:T):T =>argument;
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,10 +1,12 @@
-import { genericFunction, printObject } from './generics/generics';
+import { genericFunction, genericFunctionArrow, printObject } from './generics/generics';
 
 // printObject(123);
 // printObject(new Date());
 // printObject({a:1,b:2,c:3});
 // printObject([1,2,3,4,5,6,7,8,9,10]);
 // printObject('Hola Mundo');
+const name :string = 'Christian';
 
 console.log(genericFunction(3.14618).toFixed(2));
-console.log(genericFunction(new Date()).toFixed(2));;
+console.log(genericFunction(name).toUpperCase());
+console.log(genericFunctionArrow(new Date()).getDate());
