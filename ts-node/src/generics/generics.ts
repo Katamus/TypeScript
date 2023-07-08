c5c75f6e3587b9919fd54e2dc3a6670018ee8d54
Christian <christiancatamuscay@r-fast.com>
2023-07-08 11:34:15 -0500

    S11 - Genericos - Generics: 83. Introducción a los Genéricos

---
+++ ts-node/src/generics/generics.ts
@@ -0,0 +1,8 @@
+export const printObject  = (object:any) =>{
+    console.log(object);
+}
+
+
+export function genericFunction (argument:any){
+    return argument;
+}
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,8 +1,10 @@
-import { Hero } from "./classes/hero";
+import { genericFunction, printObject } from './generics/generics';
 
+// printObject(123);
+// printObject(new Date());
+// printObject({a:1,b:2,c:3});
+// printObject([1,2,3,4,5,6,7,8,9,10]);
+// printObject('Hola Mundo');
 
-const ironman:Hero =  new Hero("Ironman",1,55);
-
-console.log(ironman);
-console.log(ironman.power);
-
+console.log(genericFunction(3.14618).toFixed(2));
+console.log(genericFunction(new Date()).toFixed(2));;
