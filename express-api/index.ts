d8d45ccc7e09802cb2c65a1601a712af86630d28
Christian <christiancatamuscay@r-fast.com>
2023-07-08 16:48:19 -0500

    S13 - Sección 13: Usando librerías que no están escritas en TypeScript ( Como jQuery ) - 102. Trabajar con TypeScript en lugar de JavaScript

--- express-api/index.ts
+++ express-api/index.ts
@@ -1,8 +1,19 @@
-const express = require('express')
+
+import express from 'express'
+//const express = require('express')
+
+
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+
+  res.status(201).json({
+    ok:true,
+    msg:'Nuevo id: 12312123'
+  })
+
   res.json({
     ok:true,
     msg: 'Todo salio bien'
